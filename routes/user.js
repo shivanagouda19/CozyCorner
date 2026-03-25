a6423f8924a51a7d5d88da29e885434575a99999
Akshay Patil <shivanagouda941@gmail.com>
2026-03-25 16:51:14 +0530

    fixed issues

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { saveRedirectUrl, validateLogin } = require("../middleware.js");
-const { validateSignup } = require("../middleware/validateUser");
+const { validateSignup } = require("../middleware/validateUser.js");
 const userController = require("../controllers/user.js");
 
 router
